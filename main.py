efea9d21515352565ccea4278f8770bead4b7c77
mza_pl <>
2021-04-19 16:25:00 +0000

    removed unused functions

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
-from sized import Heapify, left, right, build, heapSort, div
-
-myarr = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
-# prawidłowy? wynik [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
-myarr = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
-# prawidłowy? wynik [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
-myarr = [12, 11, 13, 5, 6, 7]
-
-myarr = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
-
-myarr = [84, 22, 19, 10, 3, 17, 6, 5, 9]
+def left(i):
+    return 2 * i + 1
 
 
+def right(i):
+    return 2 * i + 2
 
-div()
-size = len(myarr)
-print("start:", myarr)
 
-div()
-wynik = build(myarr, size)
-print("build:", wynik)
+def Heapify(A, size, i):
+    largest = i
+    l = left(i)
+    r = right(i)
 
-div()
-wynik = heapSort(myarr[::], size)
-print("heapsort:", wynik)
+    if l < size and A[l] > A[i]:
+        largest = l
+    if r < size and A[r] > A[largest]:
+        largest = r
+    if i != largest:
+        A[largest], A[i] = A[i], A[largest]
+        return Heapify(A, size, largest)
+    else:
+        return A
 
-div()
+
+def build(A, size):
+    for i in range(size // 2, -1, -1):
+        A = Heapify(A, size, i)
+    return A
+
+
+def heapSort(A, size):
+    A = build(A, size)
+    for i in range(size - 1, 0, -1):
+        A[0], A[i] = A[i], A[0]
+        A = Heapify(A, i, 0)
+    return A
+
+
+def div():
+    print("\n", "-" * 30, "\n")
+
+
+def main():
+    examples = [
+        [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0],
+        [84, 22, 19, 10, 3, 17, 6, 5, 9],
+        [16, 4, 10, 14, 7, 9, 3, 2, 8, 1],
+        [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+    ]
+    for example in examples:
+        size = len(example)
+        print("start:", example)
+
+        wynik = build(example, size)
+        print("build:", wynik)
+
+        wynik = heapSort(example, size)
+        print("heapsort:", wynik)
+
+        div()
+
+
+main()
--- sized.py
+++ sized.py
@@ -1,52 +1,53 @@
-def left(i, size):
-  return 2*i+1 if 2*i+1<size else None
+def left(i):
+    return 2 * i + 1
 
-def right(i, size):
-  return 2*i+2 if 2*i+2<size else None
+
+def right(i):
+    return 2 * i + 2
 
 
 def Heapify(A, size, i):
-  print(A)
-  l = left(i, size)
-  r = right(i, size)
-  largest = i
-  if l:
-    if l< size and A[l] > A[i]:
-      largest = l
-  if r:
-    if r< size and A[r] > A[largest]:
-      largest = r
-  if i!=largest:
-    A[largest], A[i] = A[i], A[largest]
-    return Heapify(A, size, largest)
-  else:
-    return A
+    largest = i
+    l = left(i)
+    r = right(i)
+
+    if l < size and A[l] > A[i]:
+        largest = l
+
+    if r < size and A[r] > A[largest]:
+        largest = r
+
+    if i != largest:
+        A[largest], A[i] = A[i], A[largest]
+        return Heapify(A, size, largest)
+
+    else:
+        return A
+
 
 def build(A, size):
-  for i in range(size//2, -1, -1):
-    # print("\n"+"-"*15+"\n")
-    # print("Starting iteration: i=", i)
-    # print("A: ", A)
-    A = Heapify(A[::], size, i)
-    # print("Finished iteration: ", i)
-  return A
+    for i in range(size // 2, -1, -1):
+        A = Heapify(A[::], size, i)
+    return A
+
 
 def heapSort(A, size):
-  A = build(A[::], size)
-  for i in range(size-1, 0, -1):
-    A[0], A[i] = A[i], A[0]
-    A = Heapify(A[::], i, 0)
-  return A
+    A = build(A[::], size)
+    for i in range(size - 1, 0, -1):
+        A[0], A[i] = A[i], A[0]
+        A = Heapify(A[::], i, 0)
+    return A
 
 
 def div():
-  print("-"*15)
+    print("\n", "-" * 30, "\n")
 
-# def parent(i):
-#   if i!=0:
-#     if(i%2!=0):
-#       return (i//2)
-#     else:
-#       return (i//2)-1
-#   else:
-#     return None
+
+def parent(i):
+    if i != 0:
+        if i % 2 != 0:
+            return i // 2
+        else:
+            return i // 2 - 1
+    else:
+        return None
